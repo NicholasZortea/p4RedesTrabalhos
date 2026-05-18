9a569e48fef737e2395c55c58f305038c2bf3689
Nicholas Zortea Graczik <nicholas.zortea@animati.com.br>
2026-05-18 19:18:09 -0300

    Feat: Melhora aparencia da telemetria no controlador.

--- exercicios/basic/collector.py
+++ exercicios/basic/collector.py
@@ -1,33 +1,38 @@
 from scapy.all import *
 import struct
+import time
+import os
 
+class Collector:
+    def __init__(self):
+        self.last_print = 0
+
+    def process(self, pkt):
+        # apenas IPv4
+        if IP in pkt:
+            raw_bytes = bytes(pkt)
+            telemetry_offset = 14 + 20
+
+            packet_counter, bytes_counter = struct.unpack(
+                "!II",
+                raw_bytes[telemetry_offset:telemetry_offset + 8]
+            )
+            self.printValues(packet_counter, bytes_counter)
+
+    def printValues(self, packet, byte):
+        now = time.time()
+        if now - self.last_print >= 3:
+            os.system('clear')
+            print("==== TELEMETRIA ====")
+            print("Pacotes:", packet)
+            print("Bytes:", byte)
+            print()
+            self.last_print = now
+
+collector = Collector()
 def process(pkt):
+    collector.process(pkt)
 
-    # apenas IPv4
-    if IP in pkt:
-
-        raw_bytes = bytes(pkt)
-
-        #
-        # Ethernet = 14 bytes
-        # IPv4 normalmente = 20 bytes
-        #
-        telemetry_offset = 14 + 20
-
-        #
-        # Lê:
-        # packet_counter
-        # bytes_counter
-        #
-        packet_counter, bytes_counter = struct.unpack(
-            "!II",
-            raw_bytes[telemetry_offset:telemetry_offset + 8]
-        )
-
-        print("==== TELEMETRIA ====")
-        print("Pacotes:", packet_counter)
-        print("Bytes:", bytes_counter)
-        #print(raw_bytes)
-        print()
 
 sniff(iface="eth0", prn=process)
+
