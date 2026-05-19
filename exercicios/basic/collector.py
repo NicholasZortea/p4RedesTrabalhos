6749e0e2445a325a6795c73d047aa482f19842bf
Nicholas Zortea Graczik <nicholas.zortea@animati.com.br>
2026-05-18 21:06:18 -0300

    Feat: Add jitter como ultima metrica.

--- exercicios/basic/collector.py
+++ exercicios/basic/collector.py
@@ -10,24 +10,24 @@ class Collector:
             raw_bytes = bytes(pkt)
             telemetry_offset = 14 + 20
 
-            packet_counter, bytes_counter, queue_time = struct.unpack(
-                "!III",
-                raw_bytes[telemetry_offset:telemetry_offset + 12]
+            packet_counter, bytes_counter, queue_time, jitter = struct.unpack(
+                "!IIII",
+                raw_bytes[telemetry_offset:telemetry_offset + 16]
             )
-            self.printValues(packet_counter, bytes_counter, queue_time)
+            self.printValues(packet_counter, bytes_counter, queue_time, jitter)
 
-    def printValues(self, packet, byte, queue_time):
+    def printValues(self, packet, byte, queue_time, jitter):
         os.system('clear')
         print("==== TELEMETRIA ====")
         print("Pacotes:", packet)
         print("Bytes:", byte)
         print("Queue time:", queue_time)
+        print("Jitter:", jitter)
         print()
         
 collector = Collector()
 def process(pkt):
     collector.process(pkt)
-
 
 sniff(iface="eth0", prn=process)
 
