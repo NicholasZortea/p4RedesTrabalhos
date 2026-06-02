d34c56624993df4aa4b76890e187dca703d4d32a
Ramon S <ramonstudo@gmail.com>
2026-06-01 17:50:43 -0700

    Collector writes to .csv

---
+++ collector.py
@@ -0,0 +1,43 @@
+from scapy.all import *
+import struct
+import csv
+import os
+import time
+
+CSV_FILE = "telemetry.csv"
+
+if not os.path.exists(CSV_FILE):
+    with open(CSV_FILE, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow([
+            "timestamp",
+            "packets",
+            "bytes",
+            "queue_time",
+            "jitter"
+        ])
+
+class Collector:
+    def process(self, pkt):
+        if IP in pkt:
+            raw_bytes = bytes(pkt)
+            telemetry_offset = 14 + 20
+
+            packet_counter, bytes_counter, queue_time, jitter = struct.unpack(
+                "!IIII",
+                raw_bytes[telemetry_offset:telemetry_offset + 16]
+            )
+
+            with open(CSV_FILE, "a", newline="") as f:
+                writer = csv.writer(f)
+                writer.writerow([
+                    time.time(),
+                    packet_counter,
+                    bytes_counter,
+                    queue_time,
+                    jitter
+                ])
+
+collector = Collector()
+
+sniff(iface="eth0", prn=collector.process)
